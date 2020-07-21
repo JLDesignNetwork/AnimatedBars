fc624bb05df31edc25f42fa8abfc06e4a73af30e
JLDN Admin <59946223+JLDesignNetwork@users.noreply.github.com>
2020-07-21 21:01:06 +0200

    fixed issue with reveal

--- src/script.js
+++ src/script.js
@@ -1,1 +1,8 @@
-$(function() {$(".meter > span").each(function() {$(this).data("origWidth", $(this).width()).width(0).animate({width: $(this).data("origWidth")}, 1200);});});
+$(document).ready(function() {
+    $(".meter > span").each(function() {$(this).data("origWidth", $(this).width()).width(0).animate({width: $(this).data("origWidth")}, 1200);});
+    $(".meter").each(function() {
+        $("#" + $(this).attr("id") + " > span > span").css({
+            "background-image": "linear-gradient(to right, transparent " + $("#" + $(this).attr("id")).data("width") + ", #555 " + $("#" + $(this).attr("id")).data("width") + ")"
+        });
+    });
+});
