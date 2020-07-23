ce078485e42738e8e934f173ef5ca9ef3765cd9c
JLDN Admin <59946223+JLDesignNetwork@users.noreply.github.com>
2020-07-23 17:25:35 +0200

    minor error fix
    candystripe width failed to update on window resize.

--- src/script.js
+++ src/script.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
-    $(".meter > span").each(function() {$(this).data("origWidth", $(this).width()).width(0).animate({width: $(this).data("origWidth")}, 1200);});
+    $(".meter > span").each(function() {
+        $(this).data("origWidth", ( $(this).width() / $(this).parent().width()) * 100 )
+            .width(0)
+            .animate({
+                width: $(this).data("origWidth") + "%"
+            }, 3600);
+    });
+    
     $(".meter").each(function() {
         $("#" + $(this).attr("id") + " > span > span").css({
             "background-image": "linear-gradient(to right, transparent " + $("#" + $(this).attr("id")).data("width") + ", #555 " + $("#" + $(this).attr("id")).data("width") + ")"
